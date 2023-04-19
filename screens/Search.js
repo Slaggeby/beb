0ce1f5417a32b1e34a84d6085ec3bbcbec82a490
Anton <anton.mammi@gmail.com>
2023-04-19 14:46:06 +0200

    started working on a search function

--- screens/Search.js
+++ screens/Search.js
@@ -1,34 +1,155 @@
-import React, { useState } from 'react';
-import {StyleSheet, View, TextInput, Button, Text, Image, SafeAreaView, TouchableOpacity, StatusBar, ScrollView, Pressable} from "react-native"
+import React, { useState, useEffect } from 'react';
+import {SafeAreaView,Text, StyleSheet,View,FlatList,TextInput,} from 'react-native';
 
-import { ActivityIndicator } from 'react-native';
 import { collection, addDoc, getDocs } from '@firebase/firestore';
 import {database} from '../config/firebase';
 
-const fetchProducts= async ()=> {
-    await getDocs(collection(database, "products"))
-        .then((QuerySnapshot)=>{
-            const newData = querySnapshot.getDocs
-                .map((doc)=>({...doc.data(),id:doc.id }));
-                console.log(newData);
-        })
-
-fetchProducts();
 
 
-}
+
 
 
 export default function Search({navigation}) {
+    const [search, setSearch] = useState('');
+    const [filteredDataSource, setFilteredDataSource] = useState([]);
+    const [masterDataSource, setMasterDataSource] = useState([]);
+    
+    
+    const [importedDb, setImportedDb] = useState([]);
+
+
+    const fetchProducts = async () => {
+        try {
+          const querySnapshot = await getDocs(collection(database, "products"));
+          const newData = querySnapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
+         
+          setImportedDb(newData);
     
 
+          
+          console.log("Datan",newData)
+          
 
-    const [loading, setLoading] = useState(true); // Set loading to true on component mount
-    const [users, setUsers] = useState([]); // Initial empty array of users
+                
+
+
+
+        } catch (error) {
+          console.error("Error fetching products:", error);
+          throw error;
+        }
+      };
+
+      useEffect(() => {
+        fetchProducts();
+      }, []);
+
+
+
+
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/posts')
+
+          .then((response) => response.json())
+          .then((responseJson) => {
+            setFilteredDataSource(responseJson);
+            setMasterDataSource(responseJson);
+          })
+          .catch((error) => {
+            console.error(error);
+          });
+      }, []);
+
+      const searchFilterFunction = (text) => {
+        // Check if searched text is not blank
+        if (text) {
+          // Inserted text is not blank
+          // Filter the masterDataSource
+          // Update FilteredDataSource
+          const newData = masterDataSource.filter(
+            function (item) {
+              const itemData = item.title
+                ? item.title.toUpperCase()
+                : ''.toUpperCase();
+              const textData = text.toUpperCase();
+              return itemData.indexOf(textData) > -1;
+          });
+          setFilteredDataSource(newData);
+          setSearch(text);
+        } else {
+          // Inserted text is blank
+          // Update FilteredDataSource with masterDataSource
+          setFilteredDataSource(masterDataSource);
+          setSearch(text);
+        }
+      };
+      
+      const ItemView = ({item}) => {
+        return (
+          // Flat List Item
+          <Text
+            style={styles.itemStyle}
+            onPress={() => getItem(item)}>
+            {item.id}
+            {'.'}
+            {item.title.toUpperCase()}
+          </Text>
+        );
+      };
+      const ItemSeparatorView = () => {
+        return (
+          // Flat List Item Separator
+          <View
+            style={{
+              height: 0.5,
+              width: '100%',
+              backgroundColor: '#C8C8C8',
+            }}
+          />
+        );
+      };
+
+      const getItem = (item) => {
+        // Function for click on an item
+        alert('Id : ' + item.id + ' Title : ' + item.title);
+      };
+
+
+
 
 return(
-<View>
-
-</View>
+<SafeAreaView style={{flex: 1}}>
+      <View style={styles.container}>
+        <TextInput
+          style={styles.textInputStyle}
+          onChangeText={(text) => searchFilterFunction(text)}
+          value={search}
+          underlineColorAndroid="transparent"
+          placeholder="Search Here"
+        />
+        <FlatList
+          data={filteredDataSource}
+          keyExtractor={(item, index) => index.toString()}
+          ItemSeparatorComponent={ItemSeparatorView}
+          renderItem={ItemView}
+        />
+      </View>
+    </SafeAreaView>
 )
 }
+const styles = StyleSheet.create({
+    container: {
+      backgroundColor: 'white',
+    },
+    itemStyle: {
+      padding: 10,
+    },
+    textInputStyle: {
+      height: 40,
+      borderWidth: 1,
+      paddingLeft: 20,
+      margin: 5,
+      borderColor: '#009688',
+      backgroundColor: '#FFFFFF',
+    },
+  });
